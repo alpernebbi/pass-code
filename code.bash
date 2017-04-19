240837e3150beef33d4e0fe9561abaa274ad9a63
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2017-04-19 20:13:40 +0300

    Add functions to encode/decode filenames

--- code.bash
+++ code.bash
@@ -16,6 +16,35 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
+declare -A codec_dec
+declare -A codec_enc
+
+# Decrypt the .passcode file and put it to associative arrays so we
+# don't need to re-decrpyt it every time we encode/decode something.
+code_decrypt() {
+	codec=($(cmd_show .passcode))
+
+	for c in "${codec[@]}"; do
+		# Bash interprets stuff that start with digit as
+		# integers, so put an 'x' before the keys.
+		codec_enc["x${c##*:}"]="${c%%:*}"
+		codec_dec["x${c%%:*}"]="${c##*:}"
+	done
+}
+
+# Will not print anything if code_decrypt not run or key not in mappings
+code_encode() {
+	while read -r line; do
+		echo "${codec_enc[x$line]}"
+	done
+}
+
+code_decode() {
+	while read -r line; do
+		echo "${codec_dec[x$line]}"
+	done
+}
+
 cmd_code_version() {
 	cat <<- EOF
 	$PROGRAM-code version 0.1.0
