c78c9cab03cf074d3d8a78076cd857e44bd829f4
Alper Nebi Yasak <alpernebiyasak@gmail.com>
2017-04-24 21:57:24 +0300

    Make mv-tests not check files

--- tests/t0050-mv-tests.sh
+++ tests/t0050-mv-tests.sh
@@ -12,33 +12,40 @@ test_expect_success 'Basic move command' '
 	"$PASS" git init &&
 	"$PASS" insert -e cred1 <<<"$INITIAL_PASSWORD" &&
 	"$PASS" mv cred1 cred2 &&
-	[[ -e $PASSWORD_STORE_DIR/cred2.gpg && ! -e $PASSWORD_STORE_DIR/cred1.gpg ]]
+	test_must_fail "$PASS" show cred1 &&
+	"$PASS" show cred2
 '
 
 test_expect_success 'Directory creation' '
 	"$PASS" mv cred2 directory/ &&
-	[[ -d $PASSWORD_STORE_DIR/directory && -e $PASSWORD_STORE_DIR/directory/cred2.gpg ]]
+	test_must_fail "$PASS" show cred2 &&
+	"$PASS" show directory/cred2
 '
 
 test_expect_success 'Directory creation with file rename and empty directory removal' '
 	"$PASS" mv directory/cred2 "new directory with spaces"/cred &&
-	[[ -d $PASSWORD_STORE_DIR/"new directory with spaces" && -e $PASSWORD_STORE_DIR/"new directory with spaces"/cred.gpg && ! -e $PASSWORD_STORE_DIR/directory ]]
+	test_must_fail "$PASS" show directory/cred2 &&
+	"$PASS" show "new directory with spaces"/cred
 '
 
 test_expect_success 'Directory rename' '
 	"$PASS" mv "new directory with spaces" anotherdirectory &&
-	[[ -d $PASSWORD_STORE_DIR/anotherdirectory && -e $PASSWORD_STORE_DIR/anotherdirectory/cred.gpg && ! -e $PASSWORD_STORE_DIR/"new directory with spaces" ]]
+	test_must_fail "$PASS" show "new directory with spaces"/cred &&
+	"$PASS" show anotherdirectory/cred
 '
 
 test_expect_success 'Directory move into new directory' '
 	"$PASS" mv anotherdirectory "new directory with spaces"/ &&
-	[[ -d $PASSWORD_STORE_DIR/"new directory with spaces"/anotherdirectory && -e $PASSWORD_STORE_DIR/"new directory with spaces"/anotherdirectory/cred.gpg && ! -e $PASSWORD_STORE_DIR/anotherdirectory ]]
+	test_must_fail "$PASS" show anotherdirectory/cred &&
+	"$PASS" show "new directory with spaces"/anotherdirectory/cred
 '
 
 test_expect_success 'Multi-directory creation and multi-directory empty removal' '
 	"$PASS" mv "new directory with spaces"/anotherdirectory/cred new1/new2/new3/new4/thecred &&
 	"$PASS" mv new1/new2/new3/new4/thecred cred &&
-	[[ ! -d $PASSWORD_STORE_DIR/"new directory with spaces"/anotherdirectory && ! -d $PASSWORD_STORE_DIR/new1/new2/new3/new4 && -e $PASSWORD_STORE_DIR/cred.gpg ]]
+	test_must_fail "$PASS" show "new directory with spaces"/anotherdirectory/cred &&
+	test_must_fail "$PASS" show new1/new2/new3/new4/thecred &&
+	"$PASS" show cred
 '
 
 test_expect_success 'Password made it until the end' '
